d466cd17745f26c78f404725838706fb1316fb27
Lukasz Maciejewski <lukasz.m.maciejewski@pm.me>
2020-03-15 17:52:34 +0100

    fixing compilation; cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -11,39 +11,10 @@
 
 #include <docopt/docopt.h>
 
-/*
-static constexpr auto USAGE =
-  R"(Naval Fate.
 
-    Usage:
-          naval_fate ship new <name>...
-          naval_fate ship <name> move <x> <y> [--speed=<kn>]
-          naval_fate ship shoot <x> <y>
-          naval_fate mine (set|remove) <x> <y> [--moored | --drifting]
-          naval_fate (-h | --help)
-          naval_fate --version
- Options:
-          -h --help     Show this screen.
-          --version     Show version.
-          --speed=<kn>  Speed in knots [default: 10].
-          --moored      Moored (anchored) mine.
-          --drifting    Drifting mine.
-)";
-*/
-
-int main([[maybe_unused]] int argc, [[maybe_unused]] const char **argv)
+int main([[maybe_unused]] int argc, [[maybe_unused]] const char** argv)
 {
-  //  std::map<std::string, docopt::value> args = docopt::docopt(USAGE,
-  //    { std::next(argv), std::next(argv, argc) },
-  //    true,// show help if requested
-  //    "Naval Fate 2.0");// version string
-
-  //  for (auto const &arg : args) {
-  //    std::cout << arg.first << arg.second << std::endl;
-  //  }
-
-
-  //Use the default logger (stdout, multi-threaded, colored)
+  // Use the default logger (stdout, multi-threaded, colored)
   spdlog::info("Hello, {}!", "World");
 
 
@@ -55,7 +26,21 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] const char **argv)
   ImGui::GetStyle().ScaleAllSizes(scale_factor);
   ImGui::GetIO().FontGlobalScale = scale_factor;
 
-  std::array<bool, 12> states{};
+  std::vector<std::pair<std::string, bool>> states = { {
+    { "The Plan", false },
+    { "Getting Started", false },
+    { "Finding Errors As Soon As Possible", false },
+    { "Handling Command Line Parameters", false },
+    { "C++ 20 So Far", false },
+    { "Reading SFML Input States", false },
+    { "Managing Game State", false },
+    { "Making Our Game Testable", false },
+    { "Making Game State Allocator Aware", false },
+    { "Add Logging To Game Engine", false },
+    { "Draw A Game Map", false },
+    { "Dialog Trees", false },
+    { "Porting From SFML To SDL", false },
+  } };
 
   sf::Clock deltaClock;
   while (window.isOpen()) {
@@ -63,9 +48,7 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] const char **argv)
     while (window.pollEvent(event)) {
       ImGui::SFML::ProcessEvent(event);
 
-      if (event.type == sf::Event::Closed) {
-        window.close();
-      }
+      if (event.type == sf::Event::Closed) { window.close(); }
     }
 
     ImGui::SFML::Update(window, deltaClock.restart());
@@ -74,8 +57,8 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] const char **argv)
     ImGui::Begin("The Plan");
 
     int index = 0;
-    for (const auto &step : { "The Plan", "Getting Started", "Finding Errors As Soon As Possible", "Handling Command Line Parameters", "C++ 20 So Far", "Reading SFML Input States", "Managing Game State", "Making Our Game Testable", "Making Game State Allocator Aware", "Add Logging To Game Engine", "Draw A Game Map", "Dialog Trees", "Porting From SFML To SDL" }) {
-      ImGui::Checkbox(fmt::format("{} : {}", index, step).c_str(), std::next(begin(states), index));
+    for (auto& step : states) {
+      ImGui::Checkbox(fmt::format("{} : {}", index, step.first).c_str(), &step.second);
       ++index;
     }
 
